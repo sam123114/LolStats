012b894478bfe1e561e05b5cf626649edf2ae481
sam123114 <52093461+sam123114@users.noreply.github.com>
2020-11-17 17:46:01 -0500

    Update summonerNotFoundView.php

--- LolStats/HTML/summonerNotFoundView.php
+++ LolStats/HTML/summonerNotFoundView.php
@@ -1,4 +1,3 @@
-<div id="wrapper"></div>
 <div id="page-content">
     <h3 id="errorMessage">Le joueur que vous avez rechercher n'existe pas. Veuillez réessayer</h3>
     <form action="summoner.php" method="get">

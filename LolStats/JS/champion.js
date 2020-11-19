9bc8a98e40ca8b504d5a5be87cb468ad0a6a0d4c
sam123114 <52093461+sam123114@users.noreply.github.com>
2020-11-18 23:38:30 -0500

    Update champion.js

--- LolStats/JS/champion.js
+++ LolStats/JS/champion.js
@@ -1,39 +1,62 @@
-let champions;
-$(document).ready(async () => {
-    
-    let freeChampions = await fetchFreeChampionsFromAPI();
-    champions = await fetchChampionsFromAPI();
+const urlParams = new URLSearchParams(window.location.search);
+let name = urlParams.get('name');
+$(document).ready(async () =>  {
+    let infos = await fetchChampionsInfosFromAPI();
+    displayInfos(infos.data[name]);
+    console.log(infos.data[name]);
 
-    freeChampions = freeChampions.freeChampionIds.map(id => getChampionInfosFromId(id));
-    displayFreeChampions(freeChampions);
-    console.log(freeChampions);
+    $('.spell img').hover((e) => {
+        $(e.target).next('p')[0].style.display = 'block';
+    },(e) => {
+        $(e.target).next('p')[0].style.display = 'none';
+    });
 })
 
-const fetchFreeChampionsFromAPI = async () => {
-    return await fetch(`${urlFetcher}?url=https://na1.api.riotgames.com/lol/platform/v3/champion-rotations`)
+const fetchChampionsInfosFromAPI = async () => {
+    return await fetch(`http://ddragon.leagueoflegends.com/cdn/10.23.1/data/fr_FR/champion/${name}.json`)
         .then(resp => resp.json());
 }
-const fetchChampionsFromAPI = async () => {
-    return await fetch('http://ddragon.leagueoflegends.com/cdn/10.23.1/data/en_US/champion.json')
-        .then(resp => resp.json());
-}
-const getChampionInfosFromId = (championId) =>{
-    for(let champion in champions.data){
-        if(champions.data[champion].key == championId)
-            return champions.data[champion];
-    }
-}
-const displayFreeChampions = (freeChampions) => {
-
-    let display = `<div id='champions-container'>`;
-    for(let champion of freeChampions)
-        display += `<div class='championInfos'>
-                        <img src="http://ddragon.leagueoflegends.com/cdn/10.22.1/img/champion/${champion.id}.png" class="img">
-                        
-                        <h5>${champion.id}</h5>
-                        <a href='champion?name=${champion.id}' class='details'>Détails</a>
+const displayInfos = (infos) => {
+    const keys = ['Q','W','E','R'];
+    let spells = ` <div id='spells'>
+                        <div class='spell'>
+                            <img src='//opgg-static.akamaized.net/images/lol/passive/${infos.passive.image.full}'/>
+                            <p class='desc'>${infos.passive.description}</p>
+                        </div>`;
+    for(let i = 0; i < 4; ++i)
+        spells += `<div class='spell'>
+                        <img title='q' src='//opgg-static.akamaized.net/images/lol/spell/${infos.spells[i].image.full}'/>
+                        <p class='desc'>${infos.spells[i].description}</p>
+                        <span class='key'>${keys[i]}</span>
                     </div>`;
-    display += '</div>';    
-
-    $('#page-content').append(display);
+                    spells += '</div>';
+    let allyTips = '<ul>';
+    for(let i = 0; i < infos.allytips.length; ++i){
+        allyTips += `<li>${infos.allytips[i]}</li>`;
+    }
+    let enemyTips = '<ul>';
+    for(let i = 0; i < infos.enemytips.length; ++i){
+        enemyTips += `<li>${infos.enemytips[i]}</li>`;
+    }
+    enemyTips += '</ul>'
+    let content = `<div id='championHeader'>                   
+                        <img src="http://ddragon.leagueoflegends.com/cdn/10.22.1/img/champion/${name}.png" class="img">
+                        <div id="championDescription">
+                            <h2>${name}</h2>
+                            <p>${infos.lore}</p>
+                            ${spells}                
+                        </div>
+                    </div>
+                    <h2 id='tipsTitle'>Astuces</h2>
+                    <div id='tips'>
+                        <div id='ally'>
+                            <h4>Alliées</h4>
+                            ${allyTips}
+                        </div>
+                        <div id='enemy'>
+                            <h4>Ennemis</h4>
+                            ${enemyTips}
+                        </div>
+                    </div>`;
+    $('#page-content').append(content);
 }
